abc3d0554c9e5014855cbecc382d85aa9d6586ba
EdjOne <edj@users.noreply.github.com>
2026-06-06 11:02:45 +0300

    v1.15.0 — alt streets from segment.streetIDs + fix extractHouseNum

--- src/wme-google-link.user.js
+++ src/wme-google-link.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                Google Link (WME)
 // @name:uk             Google Link (WME)
-// @version             1.14.3
+// @version             1.15.0
 // @description         Search Google Places by venue address
 // @author              EdjOne
 // @match               *://www.waze.com/editor*
